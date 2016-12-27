34612f2762c86730b5ff4ce63747a9eb10b4df07
4taras4 <4taras4@gmail.com>
2016-12-27 16:50:27 +0200

    Fixed prod links

--- Hyber/Classes/SupportFiles/HyberConst.swift
+++ Hyber/Classes/SupportFiles/HyberConst.swift
@@ -18,15 +18,14 @@ let kBundleID = Bundle.main.infoDictionary?[kCFBundleIdentifierKey as String] as
 let kHyberClientAPIKey = UserDefaults.standard.string(forKey:"clientApiKey")
 let kFCM = UserDefaults.standard.string(forKey: "fcmToken")
 
-let kRegUrl = "http://46.101.192.217/mobile/api/v1/device/registration"
-let kUpdateUrl = "http://46.101.192.217/mobile/api/v1/device/update"
-let kSendMsgDr = "http://46.101.192.217/mobile/api/v1/message/dr"
-let kGetMsgHistory = "http://46.101.192.217/mobile/api/v1/message/history"
-let kSendMsgCallback = "http://46.101.192.217/mobile/api/v1/message/callback"
+let kRegUrl = "https://mobile.hyber.im/v1/device/registration"
+let kUpdateUrl = "https://mobile.hyber.im/v1/device/update"
+let kSendMsgDr = "https://push-dr.hyber.im/v1/message/dr"
+let kGetMsgHistory = "https://mobile.hyber.im/v1/message/history"
+let kSendMsgCallback = "https://push-callback.hyber.im/v1/message/callback"
 
 //** Features relise **//
-let kDeleteDevice = "http://46.101.192.217/mobile/api/v1/device/revoke"
-let kGetDeviceInfo = "http://46.101.192.217/mobile/api/v1/device/all"
+let kDeleteDevice = "https://mobile.hyber.im/v1/device/revoke"
+let kGetDeviceInfo = "https://mobile.hyber.im/v1/device/all"
 
-//46.101.192.217
-//185.46.89.20
+
